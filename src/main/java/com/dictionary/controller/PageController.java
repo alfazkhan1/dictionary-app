fe3016a6399fcf7dd6a83ee67c01404574fcfde8
alfazkhan1 <alfaazk128@gmail.com>
2026-05-20 10:58:32 +0530

    added url

---
+++ src/main/java/com/dictionary/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.dictionary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "forward:/admin.html";
+    }
+
+    @GetMapping("/member")
+    public String memberPage() {
+        return "forward:/member.html";
+    }
+}
